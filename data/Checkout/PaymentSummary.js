3a0acd32cf3c60dfc1c85ffad6b6e029546ff7fd
skinojosh22 <uduakobongebong9@gmail.com>
2025-06-28 13:39:04 +0100

    Update PaymentSummary.js

--- data/Checkout/PaymentSummary.js
+++ data/Checkout/PaymentSummary.js
@@ -1,15 +1,18 @@
-//import { cart, updateCart } from "../cart.js";
 import { getProduct } from "../products.js";
 import { getDeliveryId, deliveryOptions } from "./deliveryOption.js";
 import { formatPrice } from "../Utils/money.js";
 import { cart } from "../cart-class.js";
-import { addOrder } from "../../JS/Order.js";
+import { addOrder } from "../Orders/orders-shorts.js";
 import { businessDays } from "../Utils/BussinessDays.js";
+import {renderPaymentPage} from '../Amazon/amazon-products.js';
+import { showToast } from "../toast.js";
 
 export function paymentSummaryHTML() {
+  
   let productPrice = 0;
   let shippingFee = 0;
   let priceHTML = '';
+  
   cart.cartItems.forEach( cartItem => {
     const product = getProduct(cartItem.productId);
       productPrice += product.priceCents * cartItem.quantity;
@@ -21,51 +24,13 @@ export function paymentSummaryHTML() {
   const tax = totalBeforeTax * 0.1;
   const totalPrice = totalBeforeTax + tax;
 
-    priceHTML += `
-  <div class="payment-summary-title">
-        Order Summary
-      </div>
+ if (cart.cartItems.length === 0) {
+      priceHTML = '';
+    return;
+  } else {
+    priceHTML += renderPaymentPage(productPrice, totalPrice, totalBeforeTax, shippingFee, tax);
+  }
 
-      <div class="payment-summary-row">
-        <div>Items (<span class="item-list"></span>):</div>
-        <div class="payment-summary-money">
-        $${formatPrice(productPrice)}
-        </div>
-      </div>
-
-      <div class="payment-summary-row">
-        <div>Shipping &amp; handling:</div>
-        <div class="payment-summary-money">
-        $${formatPrice(shippingFee)}
-        </div>
-      </div>
-
-      <div class="payment-summary-row subtotal-row">
-        <div>Total before tax:</div>
-        <div class="payment-summary-money">
-        $${formatPrice(totalBeforeTax)}
-        </div>
-      </div>
-
-      <div class="payment-summary-row">
-        <div>Estimated tax (10%):</div>
-        <div class="payment-summary-money">
-        $${formatPrice(tax)}
-        </div>
-      </div>
-
-      <div class="payment-summary-row total-row">
-        <div>Order total:</div>
-        <div class="payment-summary-money">
-        $${formatPrice(totalPrice)}
-        </div>
-      </div>
-
-      <button class="place-order-button button-primary 
-      js-place-order">
-        Place your order
-      </button>
-    `;
     document.querySelector('.js-payment-summary').innerHTML = priceHTML;
     cart.updateCart();
 
@@ -81,6 +46,14 @@ export function paymentSummaryHTML() {
       const today = dayjs();
       const deliveryDate = businessDays(today, deliveryOption.deliveryTime);
 
+       if (!deliveryOption) {
+  showToast(`Missing delivery option for item:`, item);
+  return {
+    ...item,
+    deliveryDate: null
+  };
+}
+
       return {
         ...item, // spread the item fields like productId, quantity, etc.
         deliveryDate: deliveryDate.toISOString()
@@ -88,37 +61,17 @@ export function paymentSummaryHTML() {
     })
   };
 
+ 
+
+
   addOrder(order);
   cart.removeCart();
   window.location.href = 'orders.html';
 
 } catch (error) {
-  console.error('❌ An unexpected error occurred:', error);
+  showToast('❌ An unexpected error occurred:', error);
 }
    });
 
-
-
-
-
-    /*document.querySelector('.js-place-order').addEventListener('click', async ()=>{
-      try {
-        fetch('https://webhook.site/356efb3d-668b-48ac-917b-836fd8aa5029', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          cart: cart
-        })
-      })
-      const order = await response.json();
-      console.log(order);
-      addOrder(order)
-      } catch (error) {
-        console.log('An unexpected error has been detected. Please try again later')
-      }
-      
-    })*/
 
 }
