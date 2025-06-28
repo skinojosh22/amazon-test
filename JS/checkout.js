728173302a629a31b14a6b756c159ebea6c2155e
skinojosh22 <uduakobongebong9@gmail.com>
2025-06-28 13:37:30 +0100

    Update checkout.js

--- JS/checkout.js
+++ JS/checkout.js
@@ -1,20 +1,6 @@
 import { orderSummaryHTML } from "../data/Checkout/orderSummary.js";
 import { paymentSummaryHTML } from "../data/Checkout/PaymentSummary.js";
-import { loadProduct, loadProductFetch } from "../data/products.js";
 
 
-new Promise((resolve) => {
-  loadProduct(()=>{
-    resolve();
-  })
-}).then(()=>{
   orderSummaryHTML();
 paymentSummaryHTML();
-})
-
-
-/*
-loadProduct(()=>{
-orderSummaryHTML();
-paymentSummaryHTML();
-})*/
